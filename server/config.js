a7fa6d498d00f15b5020c513a5c78d462362af42
Sabin Subedi <sabinsubedi125@gmail.com>
2021-06-21 12:02:34 +0545

    Small CHnages

---
+++ server/config.js
@@ -0,0 +1,14 @@
+export default {
+  type: "service_account",
+  project_id: "hello-de203",
+  private_key_id: "ab1bbebf1efa371c2cd05623359b8852ec5a79a1",
+  private_key:
+    "-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQDlb//ERCwxu1Dx\n8T63ArDl/NpT28ImZtya3Wit2t9DF2Q6LonEY/OjmxX8Hqe+/yK5isZt20pG8jxK\n5/IjSpsX1Vlkfv9ykEDZStRDqCh9tmeqPJPHLuB9CZilGLUEf2FHJhTDaBlkL1Oo\n9je4dNSzfXWXWJ3ASOupelcmp/21C2PBsrpBTjvz9aHTm6u971DGa2TPUsnKvlfD\nrngfUVydIBMrfMX2UEXjNfON3ziWlWZMCTLIfMWXettejit0pbG7gmxQK/lEYyfe\nEQ+UL4ZIzE/tkEsbRQ3ZVY/aidnl4QXG9qHmzF6SNVFwWNpD8Mdc9OLQ5o411/Q4\n4klRpYLhAgMBAAECggEACqGdr528EYLdPZTA4/CJO2Lzx+p5LNfb1YY4ZUPsz/pL\nv+Frq1C5V6WmR2QxQCmo1YGfb7Q3VSHA1vS39NguZxSQ+5Xh5tBV3Ou5ebgXRjn3\nRD1h2JsstkaFTvMd4is32fjjAen3MTv7K6tbCh5Jw9ZmpkBqMOCAIP6E/srOd/7M\nZc2SN4n0jRIP6QJqKV54IgUKNyeGVXQdgCw7mYW8VIvwc+Sx2gHjf3vIs8Y374Jq\nCU2dIi7bTZ+neDOz/vSf6DlNxvExeIjh5V2vull0N6gVXMzFo3AmOW7RIs9WGtFQ\nuPkbG7NNCFINf4jJ9/+gdi5njgEsjNy7tM4qoKCYwQKBgQD3YdvF1arQzlW6RSdU\n7IjbG/+pe6Yii/lUHsR+5pWxvIiXVyxIYkSd0AvCK/R87HLt8xXStyTXYx7jZ27Y\nndI68/tVXpo6otWuJ66tt5L54rGA9P3nwIkKbwDgKXYFe+O3KWpkrAX05WJPYQbV\nz25Hy5WigDz3dEHN/ExvkWpBsQKBgQDtbhw0wO+jy3oDU9aOWSJi/nmfHKXjoJrT\nW5hIx6hantCDXgc2br5KBvWobmEIBZJFHdJ7diWVRN1cvOCLif803ntZxfehG0uO\n5KMozuscaszZj4A0l43dud1v7bvus+2KG3yCoBdM+0enPVhsisyaGLphpIBRsJjc\nvoun54nwMQKBgQDjCGpcK/XyYAsJQkIQjGwlOpLuBk64UeHms73+ALzNO4vX6Ob5\nH0Z91tuTvidJJu9rNbaHhhFHou5MITb3kg/IjvXQqVu+CIc2p7CGOX2n28av5SfI\ns0iRzMHu994atlKMXHQKJF3rHUGWPCO7wf0h4tNXKX3KVGW4F55Gnhv4YQKBgQDk\nSE5OWoMOlYFG2cEdNI5faBU/6qeFaNH0DckpkO5J0xDgffOpXmkPfAmOXDJnKBQu\n6DGMC1ivYHsFw4SSJah/CLrI4hLpj/jh9ur5PZnIxNcZB2RZLK52VVKXX5imNdFa\nTHs2jP5aQe/u2IjAM+Ps9YGeiC/oW0deVsaoNXLmwQKBgBAzrKMXjTKGT2b36CrL\nGBPsr5IzXfHlTmRk5ffH4jLRtF+1NHyCNcdaMawbihtuRRpY9lfMmi0aGDZr82Ip\ncFAvFTqmU18Nf4KB5w5ScdjL7sGhXpifuUkoTsal2d4RmMi5zg0ezme4bHaMJlw4\n6TrCfoLQPvSd5orTvI6aiGpS\n-----END PRIVATE KEY-----\n",
+  client_email: "firebase-adminsdk-4gyfl@hello-de203.iam.gserviceaccount.com",
+  client_id: "115167107560584306654",
+  auth_uri: "https://accounts.google.com/o/oauth2/auth",
+  token_uri: "https://oauth2.googleapis.com/token",
+  auth_provider_x509_cert_url: "https://www.googleapis.com/oauth2/v1/certs",
+  client_x509_cert_url:
+    "https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-4gyfl%40hello-de203.iam.gserviceaccount.com",
+}
---
+++ server/config_dev.js
@@ -0,0 +1,15 @@
+export default {
+  type: 'service_account',
+  project_id: 'linkdevdevelopment',
+  private_key_id: '65b277816c2be6b291e9b7d048dbb1bc29611d12',
+  private_key:
+    '-----BEGIN PRIVATE KEY-----\nMIIEvgIBADANBgkqhkiG9w0BAQEFAASCBKgwggSkAgEAAoIBAQC/J+BmzaSF+WNM\nhAazkl4ZzSV/wBPpUw0CaVRHLFz9KC/XYHo9cMDkfdNaYP+EqQExbIUer5Oq5Ewh\nUgWNMj20AR8Sy8c3t6f9aSx/hf/UwCo0enA1SXcM42ZDgPPUHpk5uC57Np9wG/Xg\nQ3ywzlKmHPZx7cIPD1yDLCA33V5UYpWdPYvR73KYhydS7QLZwIOqb5K9Gj5Cp4ct\nLGPby+OTaoxsswY1CtJMM265utsvWqfB8gSRQvh0C0WAVNPgWXCgwbYOe/YOWGHS\nnuVa8M+cpUdyBAvKsE4qK2MpjmRyW3Nw1cKx+FQll54GPblQq4OWv/U/BufuyknT\nqG1A6kJnAgMBAAECggEAWeaBLRgt9ixRvRyiojWGOBA5QJmvQ56hka7Dyi1dFlqe\nFWb0Voz+yvc6PVk9EsEBJKNrAAGHK2nvPAR9Q+iLCGs2l/pqmROw2AdsvBTjOsWd\nk8y5lPYWHWOdJsYw5mP1oCW1BpGr7mfX/T8AF48RY7pfVTmCeN66nZ5VELD1ui1K\nFjEi+HeM2hqIw3j2gL41+tfKeT69KbjVgveyQrDHA8roZwnCDLuook9UXU4r45Em\nXYlnXmmChjO/YJWlxXU3SMjfPaqqVjGCajGiCDZ/3UtnkmgdsrUEL7VHOG+1AsKb\nUlbbvI/TOgchcyhVZXYTnoO950HT0HJn8caCYi0GsQKBgQD00lJlkAYvZWQ45UxW\nbIDYKbQVyR1tlKDmB1/oqwWzGcf6EiZzFDInuRO0cHxmBUHc+7fKIVVfdKTN4hYl\nQIuMt5RjgYThr2paW4SC7ufbmFfls6pS5dSuEgm8jU/NAeRIeviv+JW/btcXFKwA\nzma/H5jJ4nKoQXwoD2yVfmrL1QKBgQDH4kOhGlaWqKsBcseLcFDfmrs2HOzD2hRj\n4tPld78dhERdvtkcjR9jAzf90r/muRBBG+lbHRRHMDgf6BdTlX82RU2dco26thu4\n7bE3ETcmL26MPMREgcOqBZ0zFGNKfazBgK85iJFHEqkb9o3yAMUIVc1p9k3j3OU1\n2S1W8srfSwKBgQDfCvpscxFfftUXDLhGiGW+Cgh6TJbV7bHEv/U96QmxSQyqpHyJ\nHV4wWGo+WrjKzYFaza18pm+8Hm3C8HZSjAJXOi0jlq00W3beSUBpdTShGKMDr3K+\nQYtI50AdmvSYBmAvNHF7CFONa3LkIT4vIdvzxdqSGw9wgPCXIq4/aEPt3QKBgAkR\nRZogVL8+5YsKRu0XakPKhCWfSEpWMUiauJwXQ482Ode62aSG7l6Sfc7A6b2wtYEO\noBZnCk10bYddAvnitUOj/zJeSPmoWzeXaMujt0m/KxenhgvJizPCT3BKw3pUmyqo\nCMrmXBjKSuue0q+gw09ufQQKjVwB4FACCyF/oB5jAoGBANyYCAPBB9fKQm/Lfui+\nD1rlE99I9FM7Zl5KS8gMCfLBw980XMv3UkAmaq3XK6nv9sKfs0CQ7aohwKPNFF2b\nuKuy2OQqM/dZ+M8V9dHGoUI2F3vwYzQvBjvhUfj0tgRP+mEVOvk6sAm3+QKrHAXl\nanQ91V2jYL4qztv3m5yq5omJ\n-----END PRIVATE KEY-----\n',
+  client_email:
+    'firebase-adminsdk-z67e8@linkdevdevelopment.iam.gserviceaccount.com',
+  client_id: '102487741686846367248',
+  auth_uri: 'https://accounts.google.com/o/oauth2/auth',
+  token_uri: 'https://oauth2.googleapis.com/token',
+  auth_provider_x509_cert_url: 'https://www.googleapis.com/oauth2/v1/certs',
+  client_x509_cert_url:
+    'https://www.googleapis.com/robot/v1/metadata/x509/firebase-adminsdk-z67e8%40linkdevdevelopment.iam.gserviceaccount.com',
+}
